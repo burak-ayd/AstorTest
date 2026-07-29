65997bcdd748e706bc6a2c1aa90c23472652cdf2
Burak Aydoğan <aydoganburak78@gmail.com>
2026-07-30 00:07:32 +0300

    test1

--- app/(tabs)/HomeScreen.jsx
+++ app/(tabs)/HomeScreen.jsx
@@ -11,7 +11,7 @@ import {
 const menuItems = [
 	{
 		id: "1",
-		label: "Trafo Kayıp",
+		label: "Trafo Kayıp 2",
 		description: "Trafo yükte kayıp hesaplayıcı",
 		key: "TrafoKayip",
 		icon: "electric-bolt",
